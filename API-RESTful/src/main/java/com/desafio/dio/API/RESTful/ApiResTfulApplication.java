9cff872e0f67367dd934d3fa5a8386d03d1f464f
PEDRO LINS <pedro.hcl29@gmail.com>
2024-06-18 16:11:29 -0300

    first commit

---
+++ API-RESTful/src/main/java/com/desafio/dio/API/RESTful/ApiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.dio.API.RESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResTfulApplication.class, args);
+	}
+
+}
